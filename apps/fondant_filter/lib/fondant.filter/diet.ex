8dcd3b9f3c88f4433c6496c158ac5466a59eafaa
Stefan Johnson <sj@scrimpycat.io>
2017-09-01 19:31:16 +1000

    Add diet struct

---
+++ apps/fondant_filter/lib/fondant.filter/diet.ex
@@ -0,0 +1,18 @@
+defmodule Fondant.Filter.Diet do
+    @moduledoc """
+      A struct representing an diet filter.
+
+      ##Fields
+
+      ###:id
+      Is the unique id to reference this filter. Is an `integer`.
+
+      ###:name
+      Is the localised name of the diet. Is a `string`.
+    """
+
+    defstruct [
+        :id,
+        :name
+    ]
+end
