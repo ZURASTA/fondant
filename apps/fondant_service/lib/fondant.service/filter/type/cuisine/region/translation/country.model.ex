9fb26779c16cd3298af14605c782a3742701ebb6
Stefan Johnson <sj@scrimpycat.io>
2017-09-10 22:10:43 +1000

    Add cuisine region country model

---
+++ apps/fondant_service/lib/fondant.service/filter/type/cuisine/region/translation/country.model.ex
@@ -0,0 +1,45 @@
+defmodule Fondant.Service.Filter.Type.Cuisine.Region.Translation.Country.Model do
+    use Ecto.Schema
+    use Translecto.Schema.Translation
+    import Ecto.Changeset
+    import Protecto
+    @moduledoc """
+      A model representing the different country names for the different
+      translations.
+
+      ##Fields
+
+      ###:translate_id
+      Is the reference to the associated group of region country entries.
+      Is an `integer`.
+
+      ###:locale_id
+      Is the reference to the specific translations for a given localisation.
+      Is an `integer`.
+
+      ###:term
+      Is the region country. Is a `string`.
+    """
+
+    schema "cuisine_region_country_translations" do
+        translation()
+        field :term, :string
+        timestamps()
+    end
+
+    @doc """
+      Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * it's a translation
+      * `term` field is required
+      * formats the `term` field as lowercase
+    """
+    def changeset(struct, params \\ %{}) do
+        struct
+        |> translation_changeset(params)
+        |> cast(params, [:term])
+        |> validate_required([:term])
+        |> format_lowercase(:term)
+    end
+end
