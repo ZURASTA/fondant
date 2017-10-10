6c42741fc7998848a594e053d9b1c8f4f4b16fc4
Stefan Johnson <sj@scrimpycat.io>
2017-10-10 19:59:57 +1100

    Create cuisine table

---
+++ apps/fondant_service/priv/repo/migrations/20171010085815_cuisine.exs
@@ -0,0 +1,27 @@
+defmodule Fondant.Service.Repo.Migrations.Cuisine do
+    use Ecto.Migration
+    import Translecto.Migration
+
+    def change do
+        create table(:cuisine_name_translations, primary_key: false) do
+            translation()
+
+            add :term, :string,
+                null: false#,
+                # comment: "The localised term for the cuisine's name"
+
+            timestamps()
+        end
+
+        create table(:cuisines) do
+            translate :name, null: false
+
+            add :region_id, references(:cuisine_regions),
+                null: false
+
+            timestamps()
+        end
+
+        create index(:cuisines, [:name], unique: true)
+    end
+end
