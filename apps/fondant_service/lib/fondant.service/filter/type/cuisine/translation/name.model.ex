6ca87b2fb3f0d381a713cce63bf157930474e26e
Stefan Johnson <sj@scrimpycat.io>
2017-10-10 20:00:15 +1100

    Create cuisine name translation model

---
+++ apps/fondant_service/lib/fondant.service/filter/type/cuisine/translation/name.model.ex
@@ -0,0 +1,45 @@
+defmodule Fondant.Service.Filter.Type.Cuisine.Translation.Name.Model do
+    use Ecto.Schema
+    use Translecto.Schema.Translation
+    import Ecto.Changeset
+    import Protecto
+    @moduledoc """
+      A model representing the different cuisine names for the different
+      translations.
+
+      ##Fields
+
+      ###:translate_id
+      Is the reference to the associated group of cuisine name entries. Is
+      an `integer`.
+
+      ###:locale_id
+      Is the reference to the specific translations for a given localisation.
+      Is an `integer`.
+
+      ###:term
+      Is the cuisine name. Is a `string`.
+    """
+
+    schema "cuisine_name_translations" do
+        translation()
+        field :term, :string
+        timestamps()
+    end
+
+    @doc """
+      Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * it's a translation
+      * `term` field is required
+      * formats the `term` field as lowercase
+    """
+    def changeset(struct, params \\ %{}) do
+        struct
+        |> translation_changeset(params)
+        |> cast(params, [:term])
+        |> validate_required([:term])
+        |> format_lowercase(:term)
+    end
+end
