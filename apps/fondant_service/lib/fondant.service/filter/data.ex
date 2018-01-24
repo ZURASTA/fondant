1ca9590f4daba40e2b9c575266de112a068cd0a1
Stefan Johnson <sj@scrimpycat.io>
2018-01-24 23:15:55 +1100

    Perform add operation of a migration

--- apps/fondant_service/lib/fondant.service/filter/data.ex
+++ apps/fondant_service/lib/fondant.service/filter/data.ex
@@ -64,12 +64,52 @@ defmodule Fondant.Service.Filter.Data do
             where: item.ref == ^ref
     end
 
-    @spec run(Yum.Migration.t, module) :: :ok | { :error, String.t }
-    defp run(migration, type) do
+    @spec prepare_translation(module, Yum.Data.translation_tree | String.t, String.t, [String.t], [[term: String.t, locale_id: integer, inserted_at: DateTime.t, updated_at: DateTime.t]]) :: [[term: String.t, locale_id: integer, inserted_at: DateTime.t, updated_at: DateTime.t]]
+    defp prepare_translation(model, translation, field \\ "term", language \\ [], inserts \\ [])
+    defp prepare_translation(model, string, field, [field|language], inserts) when is_binary(string) do
+        case Fondant.Service.Locale.to_locale_id(Enum.reverse(language) |> Enum.join("_")) do
+            nil -> inserts
+            locale -> [[term: string, locale_id: locale, inserted_at: DateTime.utc_now, updated_at: DateTime.utc_now]|inserts]
+        end
+    end
+    defp prepare_translation(_, string, _, _, inserts) when is_binary(string), do: inserts
+    defp prepare_translation(model, data, field, language, inserts) do
+        Enum.reduce(data, inserts, fn { locale, translation }, inserts ->
+            prepare_translation(model, translation, field, [to_string(locale)|language], inserts)
+        end)
+    end
+
+    @spec run(Yum.Migration.t, String.t, module, ((String.t, Yum.Data.file_filter) -> %{ optional(atom) => Yum.Data.translation_tree })) :: :ok | { :error, String.t }
+    defp run(migration, path, type, get_translations) do
         model = Module.safe_concat(type, Model)
 
         Yum.Migration.transactions(migration)
         |> Enum.reduce(Ecto.Multi.new(), fn
+            { :add, { id, ref } }, transaction ->
+                file = Enum.join([""|String.split(ref, "/", trim: true)], "/")
+                translation_fields = get_translations.(path, &(&1 == file))
+
+                Enum.reduce(translation_fields, transaction, fn { translation_field, translations }, transaction ->
+                    translation_model = Module.safe_concat(type, Translation.Name.Model)
+                    translation_head = { :add_translation_head, { ref, translation_model, translation_field } }
+
+                    case prepare_translation(translation_model, translations) do
+                        [] -> transaction
+                        [translation] -> Ecto.Multi.insert(transaction, translation_head, translation_model.changeset(struct(translation_model), Map.new(translation)))
+                        [translation|translations] ->
+                            Ecto.Multi.insert(transaction, translation_head, translation_model.changeset(struct(translation_model), Map.new(translation)))
+                            |> Ecto.Multi.merge(fn changes ->
+                                Ecto.Multi.insert_all(Ecto.Multi.new(), { :add_translation, { ref, translation_model } }, translation_model, Enum.map(translations, &([{ :translate_id, changes[translation_head].translate_id }|&1])))
+                            end)
+                    end
+                end)
+                |> Ecto.Multi.merge(fn changes ->
+                    translate_ids = Enum.reduce(changes, [], fn
+                        { { :add_translation_head, { ^ref, _, translation_field } }, %{ translate_id: translate_id } }, acc -> [{ translation_field, translate_id }|acc]
+                        _, acc -> acc
+                    end)
+                    Ecto.Multi.insert(Ecto.Multi.new(), { :add_ref, ref }, model.changeset(struct(model), Map.merge(%{ ref: ref, ref_id: id }, Map.new(translate_ids))))
+                end)
             { :move, { old_ref, new_ref } }, transaction -> Ecto.Multi.update_all(transaction, { :move_ref, { old_ref, new_ref } }, find_ref(old_ref, model), [set: [ref: new_ref]])
             { :delete, ref }, transaction ->
                 Enum.reduce(model.translations(), transaction, fn { field, translation_model }, transaction ->
