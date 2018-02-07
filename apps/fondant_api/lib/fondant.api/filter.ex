4fbbf57780bffce7d77c7f5acc7afeafd5094c8e
Stefan Johnson <sj@scrimpycat.io>
2018-02-07 19:13:58 +1100

    Expose DB management functionality to API

--- apps/fondant_api/lib/fondant.api/filter.ex
+++ apps/fondant_api/lib/fondant.api/filter.ex
@@ -1,4 +1,40 @@
 defmodule Fondant.API.Filter do
+    @moduledoc """
+      Manage the current filter dataset.
+    """
+
     @type locale :: String.t
     @type page :: integer
+
+    @service Fondant.Service.Filter
+
+    @doc """
+      Clean the database.
+
+      Removes all filters and history of the dataset.
+    """
+    @spec clean() :: :ok | { :error, String.t }
+    def clean() do
+        GenServer.call(@service, { :db, :clean })
+    end
+
+    @doc """
+      Rollback the database.
+
+      Rollback the database to the previous version of the dataset
+    """
+    @spec rollback() :: :ok | { :error, String.t }
+    def rollback() do
+        GenServer.call(@service, { :db, :rollback })
+    end
+
+    @doc """
+      Migrate the database.
+
+      Migrates the database to the latest version of the dataset.
+    """
+    @spec migrate() :: :ok | { :error, String.t }
+    def migrate() do
+        GenServer.call(@service, { :db, :migrate })
+    end
 end
